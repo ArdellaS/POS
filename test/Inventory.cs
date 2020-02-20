5766fbb4e8349d2dba1e7124b2b22b08248f4ae1
Shelton <dellashelton@quickenloans.com>
2020-02-20 13:26:44 -0500

    initial

---
+++ test/Inventory.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace POS
+{
+    public class Inventory
+    {
+        List<Product> products = new List<Product>();
+        List<Cart> cartItems = new List<Cart>();
+        public void addItem(Product p)
+        {
+            products.Add(p);
+        }
+        public void addCartItem(Cart c)
+        {
+            cartItems.Add(c);
+        }
+        public string Items(string a)
+        {
+            foreach (Product p in products)
+            {
+                if (p.Name.Contains(a))
+                {
+                    return "";
+                }
+            }
+            return a;
+        }
+        public void DisplayMenu()
+        {
+            foreach (Product p in products)
+            {
+                Console.WriteLine(p);
+            }
+        }
+        public void BuildCart()
+        {
+            double cartTotal = 0;
+            do
+            {
+                Console.Write("\nPlease select item by ID: ");
+                int customerSelection = int.Parse(Console.ReadLine());
+                Console.WriteLine("\nHow many of this item would you like?");
+                int selectionQuantity = int.Parse(Console.ReadLine());
+
+                foreach (Product p in products)
+                {
+                    if (p.Id == customerSelection)
+                    {
+                        addCartItem(new Cart(p.Name, p.Price, selectionQuantity));
+                        cartTotal = (cartTotal + (p.Price * selectionQuantity));
+                        cartTotal = cartTotal * 1.06;
+                        cartTotal = Math.Round(cartTotal, 2);
+                    }
+                }
+                
+            }
+            while (ContinueProgram());
+            Console.Clear();
+
+
+            Console.WriteLine("Receipt: ");
+            foreach (Cart c in cartItems)
+            {
+                Console.WriteLine(c);
+            }
+            Console.Write($"Sales Tax: {Math.Round((cartTotal * .06), 2)}");
+            Console.WriteLine($"\nCart Total: {cartTotal}");
+
+        }
+
+        static bool ContinueProgram()
+        {
+            char c;
+            do
+            {
+                Console.WriteLine("Would you like to continue? y/n");
+                c = Console.ReadKey().KeyChar;
+                if (c == 'n' || c == 'N')
+                {
+                    return false;
+                }
+            } while (c != 'y' && c != 'Y');
+            return true;
+        }
+
+
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace POS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            Inventory inventory = new Inventory();
+            inventory.addItem(new Product("Crunch Wrap", 3.99, "specialty", "Tasty", 1));
+            inventory.addItem(new Product("5 Layer Burrito", 4.99, "burrito", "Yummy", 2));
+            inventory.addItem(new Product("Durrito Loco Taco", 1.99, "Taco", "Durrito Shell Taco", 3));
+            inventory.addItem(new Product("Cinnabon Delights", 1.00, "value menu", "Sweet", 4));
+            inventory.addItem(new Product("Baja Blast", 2.69, "drink", "Satisfying", 5));
+            Console.WriteLine("\t\t\tWelcome to Taco Bell\n");
+            inventory.DisplayMenu();
+            inventory.BuildCart();
+        }
+    }
+}
